db4843c81f46db3324230d65680e34c52f753246
Pranav <pranavmendi@gmail.com>
2022-03-15 01:09:16 +0530

    fix: fix eslint prettier conflicts & bugs, format code

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavBar: React.FC = () => {
-	return (
-		<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-			<Container>
-				<Navbar.Brand href='#home'>ServerFlow</Navbar.Brand>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-				<Navbar.Collapse
-					id='responsive-navbar-nav'
-					className='justify-content-end'>
-					<Nav>
-						<Nav.Link href='#login'>Login</Nav.Link>
-						<Nav.Link href='#questions'>Questions</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">ServerFlow</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
+        >
+          <Nav>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#questions">Questions</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
--- src/components/NavBar/__tests__/NavBar.tsx
+++ src/components/NavBar/__tests__/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import NavBar from '../NavBar';
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import NavBar from "../NavBar";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 
-describe('Testing if navbar is rendered and buttons work correctly', () => {
-	render(<NavBar />);
-	it('Navbar is correctly rendered', () => {
-		expect(screen.getByText(/serverflow/i)).toBeInTheDocument();
-	});
+describe("Testing if navbar is rendered and buttons work correctly", () => {
+  render(<NavBar />);
+  it("Navbar is correctly rendered", () => {
+    expect(screen.getByText(/serverflow/i)).toBeInTheDocument();
+  });
 });
