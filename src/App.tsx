f4820c0870860bc7e6dc116253fbcbd59c57c636
Pranav <pranavmendi@gmail.com>
2022-04-04 03:49:33 +0530

    chore(src/app.js): setup routes and context provider in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,65 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import UserLoggedIn from './components/ProtectedRoutes/UserLoggedIn';
+import UserLoginRequired from './components/ProtectedRoutes/UserLoginRequired';
+import AppProvider from './context/AppContext';
+import Dashboard from './pages/Dashboard/Dashboard';
 import Error from './pages/Error/Error';
+import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Questions from './pages/Questions/Questions';
+import Signup from './pages/Signup/Signup';
+import SinglePost from './pages/SinglePost/SinglePost';
 
 function App() {
 	return (
-		<>
-			<NavBar />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='login' element={<Login />} />
-				<Route path='questions' element={<Questions />} />
-				<Route path='*' element={<Error text={'Page not found'} />} />
-			</Routes>
-		</>
+		<AppProvider>
+			<Toaster position='top-center' reverseOrder={false} />
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route
+						path='login'
+						element={
+							<UserLoggedIn>
+								<Login />
+							</UserLoggedIn>
+						}
+					/>
+					<Route
+						path='signup'
+						element={
+							<UserLoggedIn>
+								<Signup />
+							</UserLoggedIn>
+						}
+					/>
+					<Route path='questions' element={<Questions />} />
+					<Route path='questions/:postId' element={<SinglePost />} />
+					<Route
+						path='dashboard'
+						element={
+							<UserLoginRequired>
+								<Dashboard />
+							</UserLoginRequired>
+						}
+					/>
+					<Route
+						path='forgot-password'
+						element={<ForgotPassword />}
+					/>
+					<Route
+						path='*'
+						element={<Error text={'Page not found'} />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</AppProvider>
 	);
 }
 
