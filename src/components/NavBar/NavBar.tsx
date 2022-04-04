d0ab9a6bb65f38edbfc8b9f46137f19559e334ee
Pranav <pranavmendi@gmail.com>
2022-04-04 14:55:36 +0530

    feat: update navbar to add conditional rendering of links and handle Logout feature

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import toast from 'react-hot-toast';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import './NavBar.css';
 
 const NavBar: React.FC = () => {
+	const { user, logout } = useContext(AppContext);
+	const navigate = useNavigate();
+	const handleLogout = async () => {
+		try {
+			await logout();
+			navigate('/', { replace: true });
+		} catch (err) {
+			toast.error((err as Error).message);
+		}
+	};
 	return (
 		<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
 			<Container>
-				<Navbar.Brand href='#home'>ServerFlow</Navbar.Brand>
+				<Navbar.Brand href='https://serverflow.netlify.app/'>
+					<img src='https://i.ibb.co/GVf0jq5/logo.png' alt='logo' />
+					ServerFlow
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse
 					id='responsive-navbar-nav'
@@ -18,9 +35,22 @@ const NavBar: React.FC = () => {
 						<LinkContainer to='/questions'>
 							<Nav.Link>Questions</Nav.Link>
 						</LinkContainer>
-						<LinkContainer to='/login'>
-							<Nav.Link>Login</Nav.Link>
-						</LinkContainer>
+						{!user && (
+							<LinkContainer to='/login'>
+								<Nav.Link>Login</Nav.Link>
+							</LinkContainer>
+						)}
+						{!user && (
+							<LinkContainer to='/signup'>
+								<Nav.Link>Signup</Nav.Link>
+							</LinkContainer>
+						)}
+						{user && (
+							<LinkContainer to='/dashboard'>
+								<Nav.Link>Dashboard</Nav.Link>
+							</LinkContainer>
+						)}
+						{user && <Button onClick={handleLogout}>Logout</Button>}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
